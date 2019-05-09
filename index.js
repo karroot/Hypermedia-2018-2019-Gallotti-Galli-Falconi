b703e09c4bd28421f7128c22ed233ffee9f93de8
karroot <gabbo97pop@hotmail.it>
2019-05-09 09:51:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     path = require('path'),
     http = require('http');
 
-    let { setupDataLayer } = require("./service/DataLayer");
+    let { setupDataLayer } = require("./other/service/DataLayer");
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
@@ -14,14 +14,14 @@ let serveStatic = require("serve-static");
 // swaggerRouter configuration
 var options = {
   swaggerUi: path.join(__dirname, '/swagger.json'),
-  controllers: path.join(__dirname, './controllers'),
+  controllers: path.join(__dirname, './other/controllers'),
   useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var spec = fs.readFileSync(path.join(__dirname,'./other/api/swagger.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
-app.use(serveStatic(__dirname + "/www"));
+app.use(serveStatic(__dirname + "/public"));
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
