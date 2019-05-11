9fb1b5b136ab259f4d5a24a46107241dba0ddb7f
DavideGalli <davide11.galli@mail.polimi.it>
2019-05-11 14:11:47 +0200

    Add about page and info.js

---
+++ utils/info.js
@@ -0,0 +1,29 @@
+function getInfo() {
+    let info = {
+        "about": "This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion.<br></br> This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion.<br> This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion. This is a descriprion.    ",
+        "team": [{
+            "name": "Davide Galli",
+            "description": "Computer Science student at Politecnico di Milano.<br> Friendly neighborhood engeneer."
+        },
+        {
+            "name": "Gabriele Gallotti",
+            "description": "text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text"
+        },
+        {
+            "name": "Matteo Falconi",
+            "description": "text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text"
+        }]
+    }
+
+    $.addTemplateFormatter({
+        imgFormatter: function(value, template) {
+               let v = value.replace(" ", "").concat(".png");
+               return "./img/team/".concat(v);
+        }
+    });
+
+    $(".about-template-container").loadTemplate("#info-template", info);
+    $(".teamOne-template-container").loadTemplate("#team-template", info.team[0]);
+    $(".teamTwo-template-container").loadTemplate("#team-template", info.team[1]);
+    $(".teamThree-template-container").loadTemplate("#team-template", info.team[2]);
+     };
