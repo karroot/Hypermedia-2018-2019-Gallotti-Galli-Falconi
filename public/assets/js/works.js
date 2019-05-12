44753a9098bd6e97971926544b0aeea68daf7bcb
DavideGalli <davide11.galli@mail.polimi.it>
2019-05-12 11:53:34 +0200

    Fix work.js script

--- public/assets/js/works.js
+++ public/assets/js/works.js
@@ -4,9 +4,7 @@ function getWorks() {
     $.getJSON("../assets/works.json", function(works) {
         console.log(works); // this will show the works it in firebug console
         
-        $(".work-One").loadTemplate("#type-template", works.type[0]);
-        $(".work-Two").loadTemplate("#type-template", works.type[1]);
-        $(".work-Three").loadTemplate("#type-template", works.type[2]);
+        $(".work-One").loadTemplate("#type-template", works);
     });
 
     getFooter();
