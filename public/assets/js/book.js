86de9382754565fc9e6b4a9fd32f90efb87f2fb2
DavideGalli <davide11.galli@mail.polimi.it>
2019-05-15 00:36:28 +0200

    Implement getBookById frontend

--- public/assets/js/book.js
+++ public/assets/js/book.js
@@ -1,38 +1,49 @@
- parse = function() {
-
+ function getBooks() {
        fetch('/v2/book').then(function(response) {
               return response.json();
-      }).then(function(book) {
-              $.addTemplateFormatter({
-                     bookHrefFormatter: function(value, template) {
-                            return `https://www.google.com`;
-                     },
-                     picFormatter: function(value, template) {
-                            return `/assets/img/books/${value}.png`;
-                     },
-                     factFormatter: function(value, template) {
-                            return value.split("\n").join("<br>");
-                            
-                     },
-                     authorOneFormatter : function(value, template) {
-                            return `<small class="text-muted"> ${value}</small>`;
-                     },
-                     authorFormatter : function(value, template) {
-                            if(value!=null) {
-                                   return `<small class="text-muted">, ${value}</small>`;
-                            }
-                     }
-              });
+      }).then(parseData)      
+};    
 
-              var book1 = book.slice(0, book.length/2); 
-              var book2 = book.slice(book.length/2, book.length);
-              $(".bookCard1-template-container").loadTemplate("#template", book2, {
-                     append: true
-              });
-              $(".bookCard2-template-container").loadTemplate("#template", book1, {
-                     append: true
-              });
-              $("#btn-signup").popover();
-        })
-        
-       };    
+function parseData(book) {
+       $.addTemplateFormatter({
+              bookHrefFormatter: function(value, template) {
+                     return `/pages/book.html?id=${value}`;
+              },
+              picFormatter: function(value, template) {
+                     return `/assets/img/books/${value}.png`;
+              },
+              factFormatter: function(value, template) {
+                     return value.split("\n").join("<br>");
+                     
+              },
+              authorOneFormatter : function(value, template) {
+                     return `<small class="text-muted"> ${value}</small>`;
+              },
+              authorFormatter : function(value, template) {
+                     if(value!=null) {
+                            return `<small class="text-muted">, ${value}</small>`;
+                     }
+              }
+       });
+
+       var book1 = book.slice(0, book.length/2); 
+       var book2 = book.slice(book.length/2, book.length);
+       $(".bookCard1-template-container").loadTemplate("#template", book2, {
+              append: true
+       });
+       $(".bookCard2-template-container").loadTemplate("#template", book1, {
+              append: true
+       });
+}
+
+function getBookById() {
+       let book_id = parseQueryString(location.search).id;
+       if (!book_id) location.replace("/pages/book.html");
+       fetch(`/v2/book/${book_id}`).then(function(response) {
+              return response.json();
+      }).then(parseBookData)   
+}
+
+function parseBookData(book) {
+       
+}
