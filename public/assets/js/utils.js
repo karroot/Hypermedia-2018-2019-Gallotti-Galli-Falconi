012c0898d55fcbf1c9b4b57744a4b11062000008
DavideGalli <davide11.galli@mail.polimi.it>
2019-05-17 17:50:20 +0200

    Link post to login form

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -4,3 +4,22 @@ $("#header")
   $("#footer")
   .load('/assets/footer.html');
 }
+
+//Login
+$('#singIn').submit(function(e) {
+  e.preventDefault();
+  console.log($('#singIn').serialize().replace("%40", "@")),
+
+  $.ajax({
+
+  type: $('#singIn').attr('method'),
+  url: $('#singIn').attr('action'),
+  data: $('#singIn').serialize().replace("%40", "@"),
+  success: function(msg){
+        alert("Loggato: " + $('#singIn').serialize().replace("%40", "@"));
+  },
+  error: function(XMLHttpRequest, textStatus, errorThrown) {
+    alert("Error: email or password incorrect");
+    }
+    });
+});
