77cc613b88a71f917bd013855b6db8030ebafec0
DavideGalli <davide11.galli@mail.polimi.it>
2019-06-05 13:23:32 +0200

    Delete unnecessary files

--- public/assets/js/works.js
+++
@@ -1,8 +0,0 @@
-function getWorks() {
-    $.getJSON("../assets/works.json", function(works) {
-        console.log(works); // this will show the works it in firebug console
-        
-        $(".work-One").loadTemplate("#type-template", works);
-    });
-
-};
